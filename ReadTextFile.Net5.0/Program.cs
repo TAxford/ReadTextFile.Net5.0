34b83cbf6ba870fd3130641ffa8206e2e3fbfd9b
Timothy Axford <t.axford2@yahoo.com>
2023-04-07 08:53:34 -0400

    Added the Console Writeline to display the text file.

--- ReadTextFile.Net5.0/Program.cs
+++ ReadTextFile.Net5.0/Program.cs
@@ -7,6 +7,8 @@ namespace ReadTextFile.Net5._0
         static void Main(string[] args)
         {
             string text = System.IO.File.ReadAllText(@"C:\Users\txa334\Desktop\Assets\textFile.txt");
+
+            Console.WriteLine("Text File contains the following text: {0}", text);
         }
     }
 }
