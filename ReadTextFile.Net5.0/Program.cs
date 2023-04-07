b1b9ced9e4157396cea48b8718f58f4c0f01a68d
Timothy Axford <t.axford2@yahoo.com>
2023-04-07 08:54:09 -0400

    Added Console.ReadKey to show on the console.

--- ReadTextFile.Net5.0/Program.cs
+++ ReadTextFile.Net5.0/Program.cs
@@ -9,6 +9,7 @@ namespace ReadTextFile.Net5._0
             string text = System.IO.File.ReadAllText(@"C:\Users\txa334\Desktop\Assets\textFile.txt");
 
             Console.WriteLine("Text File contains the following text: {0}", text);
+            Console.ReadKey();
         }
     }
 }
